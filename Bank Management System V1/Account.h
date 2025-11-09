18f548e9a651a3821520bb533356866a78bbf6a5
Kumaran kaushal <kaushalkumaran0@gmail.com>
2025-11-09 11:05:13 +0530

    Main file created

---
+++ Bank Management System V1/Account.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+
+class Account
+{
+
+
+public:
+
+	//Shows menu
+	void PiggyBankMenu();
+
+	// This function create a new account
+	void CreateAccount();
+
+
+};
